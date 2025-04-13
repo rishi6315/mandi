75b8f22aa43086df2e0cd2528915555a444dae05
rishi6315 <shahrishipankajkumar@gmail.com>
2025-04-13 15:30:51 +0530

    initial commit

---
+++ src/app/mideel/mideel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mideel',
+  templateUrl: './mideel.component.html',
+  styleUrls: ['./mideel.component.css']
+})
+export class MideelComponent {
+
+}
