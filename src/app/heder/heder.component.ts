d3e5093b81ad9de305adb47cc5492ea5c47f9bea
rishi6315 <shahrishipankajkumar@gmail.com>
2025-04-19 22:48:48 +0530

    login

--- src/app/heder/heder.component.ts
+++ src/app/heder/heder.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-heder',
   templateUrl: './heder.component.html',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class HederComponent {
 
+
+  username = '';
+  password = '';
+  isLoggedIn = false;
+
+  constructor(private authService: AuthService) {
+    this.isLoggedIn = this.authService.isAuthenticated();
+  }
+
+  login() {
+    if (this.authService.login(this.username, this.password)) {
+      this.isLoggedIn = true;
+    } else {
+      alert('Invalid credentials');
+    }
+  }
+
+  logout() {
+    this.authService.logout();
+    this.isLoggedIn = false;
+    this.username = '';
+    this.password = '';
+  }
 }
